5fff2175de5b94bf6e37df8ba8e16dc493364473
ViataF <viataruth@gmail.com>
2021-03-17 16:02:40 +0200

    Updates

---
+++ precache-manifest.c3d20ce6180f0d8222d294d907e914c1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f275a2cce382a26e23c095d6e0a3bb",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "830a847cc4ad68161db4",
+    "url": "/home/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "985da236c94182036e83",
+    "url": "/home/static/css/main.69eb844f.chunk.css"
+  },
+  {
+    "revision": "830a847cc4ad68161db4",
+    "url": "/home/static/js/2.a8e99a52.chunk.js"
+  },
+  {
+    "revision": "4fef53b365fb409a8d4059530325da23",
+    "url": "/home/static/js/2.a8e99a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985da236c94182036e83",
+    "url": "/home/static/js/main.202df4a9.chunk.js"
+  },
+  {
+    "revision": "e3c899f582bcf3bd0bd1",
+    "url": "/home/static/js/runtime-main.c71ad327.js"
+  },
+  {
+    "revision": "0547178cdfb4bdca7671a354aab76b40",
+    "url": "/home/static/media/Login-system.0547178c.PNG"
+  },
+  {
+    "revision": "e3dceca503391918586c6e5257bfc94a",
+    "url": "/home/static/media/Malvie.e3dceca5.otf"
+  },
+  {
+    "revision": "e9060109c3a1b4272a79c25016bfaf95",
+    "url": "/home/static/media/github_finder.e9060109.jpg"
+  },
+  {
+    "revision": "0c4baeeb40380430589885b69a6e8cb9",
+    "url": "/home/static/media/it-logger.0c4baeeb.PNG"
+  },
+  {
+    "revision": "c0e3f20cc1f183eb32671360e31483fc",
+    "url": "/home/static/media/login.c0e3f20c.PNG"
+  },
+  {
+    "revision": "516b9a52c41d90ccb91908bde24e89f4",
+    "url": "/home/static/media/lotto.516b9a52.PNG"
+  },
+  {
+    "revision": "cce248863379ffd313ad42ea234a358e",
+    "url": "/home/static/media/me1.cce24886.jpg"
+  },
+  {
+    "revision": "7b8b18ab037ddf219c65622186792f98",
+    "url": "/home/static/media/seekers&finders.7b8b18ab.PNG"
+  }
+]);
